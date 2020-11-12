fb079ed9dab3620181c8f43e627ad1a85949fbdf
TimLayne93 <65874282+TimLayne93@users.noreply.github.com>
2020-11-11 19:26:49 -0500

    Web Permission Modal
    Insert component with: function to do on yes/no and a managed true/false state to show/hide modal.

---
+++ WebPermisModal.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {View, TouchableHighlight, Text, Modal} from 'react-native';
+
+
+export default function WebPermisModal({onPressYes,onPressNo, webVisible}){
+
+    return(
+
+        <Modal 
+      animationType = {'slide'}
+      visible = {webVisible}
+      transparent = {true}>
+      <View style = {{flex:1, justifyContent: 'center', alignItems:  'center' }}>
+        <View style = {{backgroundColor: 'white', height: 200, width: 200, flexDirection: 'column',alignItems: 'center', justifyContent: 'center', borderWidth: 5, borderColor: 'black', borderRadius: 25, }}>
+          <Text style = {{marginTop: 35, flex:1}}>Test</Text>
+          <View style = {{flex:1,flexDirection: 'row',alignItems: 'center'}}>
+            <View style = {{flex:1,justifyContent: 'center', alignItems:'center'}}>
+              <TouchableHighlight onPress = {onPressYes}>
+                <Text style >Yes</Text>
+              </TouchableHighlight>
+            </View>  
+            <View style = {{flex:1,justifyContent: 'center', alignItems:'center'}}>
+              <TouchableHighlight onPress = {onPressNo}>
+                <Text style >No</Text>
+              </TouchableHighlight>
+            </View>  
+          </View>
+        </View>
+      </View>
+    </Modal>
+
+
+    )
+
+}
