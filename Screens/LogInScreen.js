8dc8c9ee93e038ece4fa429b071e40061b868d71
TimLayne93 <65874282+TimLayne93@users.noreply.github.com>
2020-12-01 18:21:53 -0500

    Update LogInScreen.js

--- Screens/LogInScreen.js
+++ Screens/LogInScreen.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { addUser, isUser} from '../Components/Redux/AlanaDataActions';
 
 import SocialLogin from '../Components/SocialLogins.js';
 import RegistrationForm from '../Components/RegistrationForm.js';
-export default function LogInScreen({navigation,route}){
+
+ function LogInScreen(props){
 
     return(
 
         <View style = {styles.main}> 
             
-           
                 <RegistrationForm/>
-                <SocialLogin navigation= {navigation} route = {route}/>
+                <SocialLogin 
+                navigation = {props.navigation} 
+                reducer = {props.AlanaDataReducer}
+                reducerAction = {props.addUser}
+                />
            
 
          </View>       
@@ -37,3 +44,13 @@ const styles = StyleSheet.create({
 
 
 })
+const mapStateToProps = (state) => {
+    const { AlanaDataReducer } = state
+    return { AlanaDataReducer }
+  };
+  const mapDispatchToProps = dispatch => (
+    bindActionCreators({
+      addUser,
+    }, dispatch)
+  );
+  export default connect(mapStateToProps,mapDispatchToProps)(LogInScreen);
