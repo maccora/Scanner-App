aad76db2c801b4d67af6f8aa114d1667689c934e
Tim Layne <65874282+TimLayne93@users.noreply.github.com>
2021-01-04 07:42:09 -0500

    Update RegistrationForm.js

--- Components/RegistrationForm.js
+++ Components/RegistrationForm.js
@@ -1,111 +1,84 @@
-import React,{useState} from 'react';
-import {View, Text, StyleSheet,TextInput,Button} from 'react-native';
-import { set } from 'react-native-reanimated';
+import React,{useState, useRef} from 'react';
+import {View, StyleSheet,TextInput,Button, Modal} from 'react-native';
 
+import RegisterUserModal from './RegisterUserModal';
 import AlanaData from '../AlanaData.json';
 
-export default  RegistrationForm = ({navigation}) =>{
-    const [email, setEmail] = useState(null);
-    const [passWord, setPass] = useState(null);
-    const [showRegister, setShow] = useState(false)
-    function isUser(email,arrayUsers){
 
-        for(let i = 0; i <arrayUsers.length;i++ )
-        {
-            
-            if(arrayUsers[i].email == email)
-            {
-               
-                return {"index": i, "exists": true}
-            }
-            else{
-                 
-            }
-            
-        }
-        
-         return {"index": null, "exists": false}
-  }
-    const submitForm = ()=>
-    {
-            setShow(!showRegister)
-            const userData = isUser(email,AlanaData.users)
-            if(!userData.exists)
-            {
-                AlanaData.users.push({"email": email, "password": passWord })
-            }
-            
-                 
-    }
-    function validateForm()
-    {
-        console.log(email,passWord)
-        let userData = isUser(email,AlanaData.users)
-        console.log(userData)
-        if(userData.exists && AlanaData.users[userData.index].passWord == passWord )
-        {
-            alert("true")
-            return {"type": "success", "validated": true}
-        }
-        else if(!userData.exists){
+export default function RegistrationForm({navigation, reducers, reducerActions}){
 
-            alert("User does not exist")  
-            return {"type": "null", "validated": false}
-
-        }
-        else{
-            
-            alert("Incorrect password or email")
-            return {"type": "mismatch","validated": false}
-            
-        }
-    }
+    const [email, setEmail] = useState("");
+    const [password, setPass] = useState("");
+    const [showRegister, setRegister] = useState(false);
+    
     function handleLogIn()
-    {
-        const isValid = validateForm()
-        //console.log(isValid)
-        if(isValid.validated)
+    {   
+        reducerActions.validateUser({email:email, password:password})
+        const userValidation = reducers.authReducer.existAndValid
+    
+        if(userValidation.isValidated)
         {
-            navigation.navigate("TabNavigator", {"userID": user.email})
+
+            reducerActions.logUserIn(email)
+            reducerActions.readUserFromDatabase(email)
+            navigation.navigate("TabNavigator")
+
         }
         else{
-            
+            alert(userValidation.message)
         }
+    
     }
-             
+    
+    function register()
+    {
+        modalRef.toggleVisible()
+    }
     return(
 
         <View style = {styles.container}>
              
-             {showRegister?
                 <View style = {{flex:1,alignItems: 'center', justifyContent: 'center'}}>
-                    <TextInput  onChangeText = {text => setEmail(text)} value = {email}clearTextOnFocus = {true} placeholder = {"Phone Number or Email"}/>
-                    <TextInput  onChangeText = {text => setPass(text)} value = {passWord} clearTextOnFocus = {true} placeholder = {"Password"}/>
-                    <Button title = "Register"onPress = {()=>{submitForm}}/>
-                   
+
+                    <TextInput 
+
+                        style  = {{height: 40,width:200, borderBottomWidth: 2.5,}}  
+                        onChangeText = {text => setEmail(text)} 
+                        textAlign = {"center"}
+                        value = {email}
+                        clearTextOnFocus = {true}
+                        placeholder = {"Email"}/>
+
+                    <TextInput 
+                                      
+                        style = {{height: 40, width:200,}}
+                        onChangeText = {text => setPass(text)} 
+                        textAlign = {"center"}
+                        value = {password} 
+                        clearTextOnFocus = {true} 
+                        secureTextEntry = {true}
+                        placeholder = {"Password"}/>
+               
                 </View>
-                    :
-                <View style = {{flex:1,alignItems: 'center', justifyContent: 'center'}}>
-                    <TextInput  onChangeText = {text => setEmail(text)} value = {email}clearTextOnFocus = {true} placeholder = {"Phone Number or Email"}/>
-                    <TextInput  onChangeText = {text => setPass(text)} value = {passWord}clearTextOnFocus = {true}placeholder = {"Password"}/>
-                    <Button title = "Log In"onPress = {()=>{handleLogIn}}/>
-                
-                </View>}
-         
+                <View style = {{flex:2, flexDirection: 'row', justifyContent:'center', alignContent:'center'}}>
+                    <View style = {{justifyContent:'center', alignContent:'center', margin:10,}}>
+                        
+                        <Button title = "Log In"onPress = {()=>{handleLogIn()}}/>
+                    </View>
+                    <View style = {{justifyContent:'center', alignContent:'center', margin:10}}>
+                        <Button title = "Register"onPress = {()=>{register()}}/>
+                    </View>  
+                </View> 
+                <View>
+                    <RegisterUserModal ref = {ref=> {modalRef = ref}} reducerActions = {reducerActions} reducers = {reducers}/>
+                </View>    
         </View>
 
     )
-    
-        
-
-
-    
-
 }
 
 const styles = StyleSheet.create({
-
-   
+ 
     container:{
         flex:1,
         alignContent:'center',
@@ -113,14 +86,11 @@ const styles = StyleSheet.create({
       
     },
     input:{
-
        
         flex:1,
         alignContent: 'center',
         justifyContent: 'center',
         
-
     },
     
-
 })
