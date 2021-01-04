ba8a746540136b860240d30a4f4b29e712176246
Tim Layne <65874282+TimLayne93@users.noreply.github.com>
2021-01-04 07:49:01 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import StackNavigator from './Navigators/StackNavigator';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import AlanaDataReducer from './Components/Redux/AlanaDataReducer.js';
+import rootReducer from './Components/Redux/RootReducer.js';
 
-
-const store = createStore(AlanaDataReducer);
+const store = createStore(rootReducer);
 
 export default function App(){
  
   return(
+    
         <Provider store = {store}>
-          <StackNavigator/>
+         <StackNavigator/>
         </Provider> 
   )
     
