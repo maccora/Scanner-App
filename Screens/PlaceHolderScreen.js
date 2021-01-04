84a9884a1a5cf78ca7d8f59a1fe6c3e4b5d34840
Tim Layne <65874282+TimLayne93@users.noreply.github.com>
2021-01-04 07:48:00 -0500

    Create PlaceHolderScreen.js

---
+++ Screens/PlaceHolderScreen.js
@@ -0,0 +1,16 @@
+import React, {useRef, useEffect, useState} from 'react';
+import {Animated,View,SafeAreaView, Text, StyleSheet, Button, Image} from 'react-native';
+
+export default function PlaceHolderScreen(){
+  
+
+  return(
+    <View style = {{flex:1, justifyContent:'center', alignItems: 'center'}}>
+
+        <Text>Test</Text>
+   
+    </View>
+   
+  )
+}
+
