f60783df81942ec418413ac1a9e1b0d3e36a6916
Tim Layne <65874282+TimLayne93@users.noreply.github.com>
2021-01-04 07:46:37 -0500

    Update ProfileScreen.js

--- Screens/ProfileScreen.js
+++ Screens/ProfileScreen.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {View,Text} from 'react-native';
+import {connect} from 'react-redux';
 
-export default function ProfileScreen({navigation,route}){
+export default function ProfileScreen({navigation,route,props}){
 
   return( 
     
     <View style = {{flex:1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>{console.log(route.params)}</Text>
+        <Text>test</Text>
     </View>
   
   )
