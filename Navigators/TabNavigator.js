81488e1cbf6ee700225a565541631d3d1e14a7be
Tim Layne <65874282+TimLayne93@users.noreply.github.com>
2021-01-05 04:52:19 -0500

    Update TabNavigator.js

--- Navigators/TabNavigator.js
+++ Navigators/TabNavigator.js
@@ -1,28 +1,152 @@
-import React from 'react';
-import {View, TouchableOpacity,Text, StyleSheet} from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-import CustomTabBar from '../Components/CustomTabBar.js';
-import HistoryScreen from '../Screens/HistoryScreen.js';
-import CodeCaptureScreen from '../Screens/CodeCaptureScreen.js';
-import ProfileScreen from '../Screens/ProfileScreen';
-import PlaceHolderScreen from '../Screens/PlaceHolderScreen';
-
-export default function TabNavigator() {
+import { useLinkProps } from '@react-navigation/native';
+import React, {useState,useEffect} from 'react';
+import {View, TouchableOpacity,Text, StyleSheet, Image, ImageBackground } from 'react-native';
 
 
+function fillElements(tabComponents, iconComponentArray, state, descriptors, navigation)
+  {
+   
+    
+    for(let i = 0; i < state.routes.length;i++)
+    {
+     
+                          
+      const { options } = descriptors[state.routes[i].key];
+            const label =
+              options.tabBarLabel !== undefined
+                ? options.tabBarLabel
+                : options.title !== undefined
+                ? options.title
+                : state.routes[i].name;
+    
+            const isFocused = state.index === i;
+    
+            const onPress = () => {
+              const event = navigation.emit({
+                type: 'tabPress',
+                target: state.routes[i].key,
+              });
+    
+              if (!isFocused && !event.defaultPrevented) {
+                navigation.navigate(state.routes[i].name);
+              }
+            };
+    
+            const onLongPress = () => {
+              navigation.emit({
+                type: 'tabLongPress',
+                target: state.routes[i].key,
+              });
+            };
+            
+            tabComponents[state.routes[i].name] = <TouchableOpacity onPress = {onPress}>{iconComponentArray[i]}</TouchableOpacity>
+           
+    }
+  }
+
+export default function CustomTabBar({ state, descriptors, navigation }) {
+    
+    const tabElements = {
+        CodeCapture:null,
+        History: null,
+        middle: <TouchableOpacity onPress = {()=>{middleButton(state,navigation)}}>
+                  <Image style = {styles.logo} source ={require("../assets/Aliza-Logo.png")}/>
+                </TouchableOpacity>,
+        ProfileScreen: null,
+        PlaceHolderScreen:null,
+
+    }
+    function middleButton(state,navigation)
+    {
+        const lastItem = state.history.length-1
+        const lastRoute = state.history[lastItem].key
+       
+        if(lastRoute.indexOf("CodeCapture")!= -1)
+        {
+           navigation.setParams({
+             action: "snap"
+           })
+        }
+
+        else{
+
+          navigation.navigate("CodeCapture")
+        }
+        
+    }
+
+    const tabIconComponents = [<Image  style = {styles.icon} source ={require("../assets/AlizaHome.png")}/>,
+                                <Image  style = {styles.icon} source ={require("../assets/AlizaProfile.png")}/>,
+                               <Image  style = {styles.icon} source ={require("../assets/AlizaFavorites.png")}/>,
+                               <Image  style = {styles.icon} source ={require("../assets/AlizaRefreshBag.png")}/>]
+
+    fillElements(tabElements,tabIconComponents,state,descriptors,navigation)
   
-  const Tab = createBottomTabNavigator();
-  return (
-      
-      <Tab.Navigator tabBar = {props=> <CustomTabBar {...props}/>}>
+    return (
 
-        <Tab.Screen name="CodeCapture" component={CodeCaptureScreen} initialParams = {{action: ''}}/>
-        <Tab.Screen name="ProfileScreen" component={ProfileScreen}/>
-        <Tab.Screen name="History" component={HistoryScreen}/>
-        <Tab.Screen name="PlaceHolderScreen" component={PlaceHolderScreen}/>
+        <View>
+        <ImageBackground style = {styles.outerRow} source = {require("../assets/AlizaBackground.png")}>
+          <View style = {styles.innerRow}>
+            <View style = {{marginRight:10}}>
+                {tabElements["CodeCapture"]}
+            </View>
+            <View style = {{marginLeft:10}}>
+                {tabElements["ProfileScreen"]}
+            </View>
+        </View>
+        <View style = {styles.innerRow}>
+            {tabElements["middle"]}
+        </View>
+        <View style = {styles.innerRow}>
+            <View style = {{marginRight:10}}>
+                {tabElements["History"]}
+            </View>
+            <View style = {{marginLeft:10}}>
+                {tabElements["PlaceHolderScreen"]}
+            </View>
+        </View>
+        </ImageBackground>   
+      </View> 
+   
+       
+     
+    );
+  }
+
+  const styles = StyleSheet.create({
+
+    outerRow:{
+
+      height:85,
+      backgroundColor: 'black',
+      justifyContent:'center',
+      alignItems:'center',
+      flexDirection: 'row',
+
+    },
+
+    innerRow:{
+  
+      flex:1, 
+      flexDirection: 'row',
+      justifyContent: 'space-evenly', 
+      alignItems: 'center',
       
-      </Tab.Navigator>
       
-  );
-}
+    },
+    
+    logo:{
+      position: 'absolute',
+      top: -70,
+      left: -45,
+      height:100,
+      width:95,
+
+    },
+    icon:{
+    
+        height:40,
+        width:40,
+    }
+  
+  })
